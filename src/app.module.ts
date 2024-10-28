11ee1fce0284e1a54d04b58e37bf583fbb70b00c
rahulpurohit186@gmail.com <rahulpurohit186@gmail.com>
2024-10-29 01:07:23 +0530

    Adding ConfigModule to Access EnvFile

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AllModules } from './modules';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/flydrone'),
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/flydrone'),
     ...AllModules
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
